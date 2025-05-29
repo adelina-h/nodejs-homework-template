6bdacf17b422958da4b11ea0df63daa1a3d06b68
Adelina-hub <adelina.macarascu@gmail.com>
2025-05-29 15:06:18 +0300

    config

---
+++ server copy.js
@@ -0,0 +1,20 @@
+const app = require("./app");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running. Use our API on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error(`Failed to start server: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+start();
